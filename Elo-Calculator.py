e8b1cca8c4cc9b16448252bfbe958bf1f65ad5c7
p1kac1u <67856006+p1kac1u@users.noreply.github.com>
2020-07-05 14:37:21 +0600

    Add files via upload

---
+++ Elo-Calculator.py
@@ -0,0 +1,34 @@
+#Elo-Calculator.py
+'''
+Copyright 2020 Supratik Saha
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+import math
+
+#Input current ratings
+r1 = int(input("Enter your current ratings: "))
+r2 = int(input("Enter your opponent's current ratings: "))
+k = int(input("Enter the k-factor: "))
+
+#Input Score
+s1 = int(input("Enter your score: "))
+s2 = int(input("Enter your opponent's score: "))
+
+#Calculate Expected Ratings
+exp1 = 1 / (1 + 10 ** ((r2-r1) / 400))
+exp2 = 1 / (1 + 10 ** ((r1-r2) / 400))
+
+#Calculate New Ratings
+nr1 = (r1 + (s1-exp1)*k)
+nr2 = (r2 + (s2-exp2)*k)
+
+#Print the new ratings with rounded
+nr1 = math.floor(nr1)
+nr2 = math.floor(nr2)
+print(nr1)
+print(nr2)
